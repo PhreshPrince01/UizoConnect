e6c0b175ec8620a7294b8f1efe4391baf32e0712
Prince <pmelane023@student.wethinkcode.co.za>
2024-10-26 11:50:50 +0200

    Added campaign on the database

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
+import os
 from . import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin  # Import UserMixin
+from werkzeug.utils import secure_filename
+from flask_login import UserMixin
 
-class User(db.Model, UserMixin):  # Inherit from UserMixin
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
@@ -16,3 +18,20 @@ class User(db.Model, UserMixin):  # Inherit from UserMixin
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+
+class Campaign(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    campaign_name = db.Column(db.String(150), nullable=False, unique=True)
+    description = db.Column(db.String(1500), nullable=False, unique=True)
+    date_started = db.Column(db.DateTime, default=datetime.utcnow)
+    total_funds = db.Column(db.Float, nullable=False)
+    ID_document = db.Column(db.String(150))
+    doctors_letter = db.Column(db.String(150))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+    def save_file(file):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join('uploads', filename)
+        file.save(file_path)
+        return file_path
